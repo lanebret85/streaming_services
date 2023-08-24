c22f07a5c6b135e49f0bc9cc55db9b62f7ce5cd4
Lane Bretschneider <lanebret85@gmail.com>
2023-08-24 13:16:25 -0700

    test: add streaming service model tests for relationships and validations

--- spec/models/streaming_service_spec.rb
+++ spec/models/streaming_service_spec.rb
@@ -1,5 +1,16 @@
 require "rails_helper"
 
 RSpec.describe StreamingService, type: :model do
+  describe 'relationships' do
+    it { should have_many :shows }
+  end
 
+  describe 'validations' do
+    it { should validate_presence_of :name }
+    it { should allow_value(true).for(:subscribed)}
+    it { should allow_value(false).for(:subscribed)}
+    it { should allow_value(true).for(:logged_in)}
+    it { should allow_value(false).for(:logged_in)}
+    it { should validate_presence_of :rating }
+  end
 end
