120359b658aa5b886c8ad270b5b7445a5bbb4335
Lane Bretschneider <lanebret85@gmail.com>
2023-08-24 12:51:35 -0700

    add 3 streaming services to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+netflix = StreamingService.create!(name: "Netflix", subscribed: true, logged_in: true, rating: 42)
+hulu = StreamingService.create!(name: "Hulu", subscribed: true, logged_in: true, rating: 39)
+disney_plus = StreamingService.create!(name: "Disney Plus", subscribed: true, logged_in: false, rating: 35)
+
