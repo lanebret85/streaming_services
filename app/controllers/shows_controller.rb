7b49d027c6ddaf8f214632b9ad6edf1337c5bc6b
Lane Bretschneider <lanebret85@gmail.com>
2023-08-24 12:50:08 -0700

    add index action to shows_controller

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,3 +1,17 @@
 class ShowsController < ApplicationController
-
+  def index
+    @shows = Show.all
+    if Show.find(params[:id]).released
+      @released_y_n = "Yes"
+    else
+      @released_y_n = "No"
+    end
+    if Show.find(params[:id]).streaming_service_id == 1
+      @streaming_service = "Netflix"
+    elsif Show.find(params[:id]).streaming_service_id == 2
+      @streaming_service = "Hulu"
+    elsif Show.find(params[:id]).streaming_service_id == 3
+      @streaming_service = "Disney Plus"
+    end
+  end
 end
