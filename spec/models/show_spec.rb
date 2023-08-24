90f1c2bfcc1baa9ae0c6dd9928d4564bc2954062
Lane Bretschneider <lanebret85@gmail.com>
2023-08-24 13:16:54 -0700

    test: add show model tests for relationships and validations

--- spec/models/show_spec.rb
+++ spec/models/show_spec.rb
@@ -1,5 +1,17 @@
 require "rails_helper"
 
 RSpec.describe Show, type: :model do
+  describe 'relationships' do
+    it { should belong_to :streaming_service }
+  end
 
+  describe 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :genre }
+    it { should allow_value(true).for(:released)}
+    it { should allow_value(false).for(:released)}
+    it { should validate_presence_of :episodes }
+    it { should validate_presence_of :seasons }
+    it { should validate_presence_of :episode_runtime }
+  end
 end
