b890253678bb336bfac9bf9f58371ef540dacc31
Lane Bretschneider <lanebret85@gmail.com>
2023-08-24 18:29:10 -0700

    feat: add validates statements to Show model

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,10 @@
 class Show < ApplicationRecord
   belongs_to :streaming_service
 
+  validates_presence_of :name
+  validates_presence_of :genre
+  validates :released, inclusion: [true, false]
+  validates_presence_of :episodes
+  validates_presence_of :seasons
+  validates_presence_of :episode_runtime
 end
