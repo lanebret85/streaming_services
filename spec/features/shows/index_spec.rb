fe9176bbedf78bc6de240ad30c84d30c0952b90b
Lane Bretschneider <lanebret85@gmail.com>
2023-08-24 18:34:09 -0700

    feat: add before each with streaming services, fix: modify to call @streaming_service.shows.create, refactor: test for match more readable html in shows index view

--- spec/features/shows/index_spec.rb
+++ spec/features/shows/index_spec.rb
@@ -1,6 +1,11 @@
 require "rails_helper"
 
 RSpec.describe "Shows Index", type: :feature do
+  before :each do
+    @netflix = StreamingService.create!(name: "Netflix", subscribed: true, logged_in: true, rating: 42)
+    @hulu = StreamingService.create!(name: "Hulu", subscribed: true, logged_in: true, rating: 39)
+    @disney_plus = StreamingService.create!(name: "Disney Plus", subscribed: true, logged_in: false, rating: 35)
+  end
   # As a visitor
   # When I visit '/child_table_name'
   # Then I see each Child in the system including the Child's attributes
@@ -9,34 +14,34 @@ RSpec.describe "Shows Index", type: :feature do
     describe "When I visit '/shows'" do
       it "can display all shows in the system with their attributes" do
         # arrange
-        the_witcher = Show.create!(name: "The Witcher", genre: "Fantasy", released: true, episodes: 24, seasons: 3, episode_runtime: 60, streaming_service_id: 1)
-        is_it_cake = Show.create!(name: "Is it Cake?", genre: "Competition", released: true, episodes: 20, seasons: 2, episode_runtime: 60, streaming_service_id: 1)
-        new_girl = Show.create!(name: "New Girl", genre: "Sitcom", released: true, episodes: 146, seasons: 7, episode_runtime: 30, streaming_service_id: 2)
+        the_witcher = @netflix.shows.create!(name: "The Witcher", genre: "Fantasy", released: true, episodes: 24, seasons: 3, episode_runtime: 60)
+        is_it_cake = @netflix.shows.create!(name: "Is it Cake?", genre: "Competition", released: true, episodes: 20, seasons: 2, episode_runtime: 60)
+        new_girl = @hulu.shows.create!(name: "New Girl", genre: "Sitcom", released: true, episodes: 146, seasons: 7, episode_runtime: 30)
         # act
         visit "/shows"
         # assert
         expect(page).to have_content("All Shows")
         expect(page).to have_content(the_witcher.name)
         expect(page).to have_content("Genre: #{the_witcher.genre}")
-        expect(page).to have_content("Released: Yes")
-        expect(page).to have_content("Episodes: #{the_witcher.episodes}")
-        expect(page).to have_content("Seasons: #{the_witcher.seasons}")
-        expect(page).to have_content("Episode Runtime: #{the_witcher.episode_runtime}")
-        expect(page).to have_content("Streaming Service: Netflix")
+        expect(page).to have_content("Released: #{the_witcher.released}")
+        expect(page).to have_content("#{the_witcher.seasons} Seasons")
+        expect(page).to have_content("#{the_witcher.episodes} Episodes")
+        expect(page).to have_content("#{the_witcher.episode_runtime} Minute Episodes")
+        expect(page).to have_content("Streaming Service: #{the_witcher.streaming_service_id}")
         expect(page).to have_content(is_it_cake.name)
         expect(page).to have_content("Genre: #{is_it_cake.genre}")
-        expect(page).to have_content("Released: Yes")
-        expect(page).to have_content("Episodes: #{is_it_cake.episodes}")
-        expect(page).to have_content("Seasons: #{is_it_cake.seasons}")
-        expect(page).to have_content("Episode Runtime: #{is_it_cake.episode_runtime}")
-        expect(page).to have_content("Streaming Service: Netflix")
+        expect(page).to have_content("Released: #{is_it_cake.released}")
+        expect(page).to have_content("#{is_it_cake.seasons} Seasons")
+        expect(page).to have_content("#{is_it_cake.episodes} Episodes")
+        expect(page).to have_content("#{is_it_cake.episode_runtime} Minute Episodes")
+        expect(page).to have_content("Streaming Service: #{is_it_cake.streaming_service_id}")
         expect(page).to have_content(new_girl.name)
         expect(page).to have_content("Genre: #{new_girl.genre}")
-        expect(page).to have_content("Released: Yes")
-        expect(page).to have_content("Episodes: #{new_girl.episodes}")
-        expect(page).to have_content("Seasons: #{new_girl.seasons}")
-        expect(page).to have_content("Episode Runtime: #{new_girl.episode_runtime}")
-        expect(page).to have_content("Streaming Service: Hulu")
+        expect(page).to have_content("Released: #{new_girl.released}")
+        expect(page).to have_content("#{new_girl.seasons} Seasons")
+        expect(page).to have_content("#{new_girl.episodes} Episodes")
+        expect(page).to have_content("#{new_girl.episode_runtime} Minute Episodes")
+        expect(page).to have_content("Streaming Service: #{new_girl.streaming_service_id}")
       end
     end
   end
