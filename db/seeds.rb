7b4e896e14572876166889c0186e119a7712f900
Lane Bretschneider <lanebret85@gmail.com>
2023-08-24 21:22:00 -0700

    modify episodes and episode_runtime attributs for is_it_cake

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,5 +11,5 @@ hulu = StreamingService.create!(name: "Hulu", subscribed: true, logged_in: true,
 disney_plus = StreamingService.create!(name: "Disney Plus", subscribed: true, logged_in: false, rating: 35)
 
 the_witcher = netflix.shows.create!(name: "The Witcher", genre: "Fantasy", released: true, episodes: 24, seasons: 3, episode_runtime: 60)
-is_it_cake = netflix.shows.create!(name: "Is It Cake?", genre: "Competition", released: true, episodes: 20, seasons: 2, episode_runtime: 60)
+is_it_cake = netflix.shows.create!(name: "Is It Cake?", genre: "Competition", released: true, episodes: 16, seasons: 2, episode_runtime: 40)
 new_girl = hulu.shows.create!(name: "New Girl", genre: "Sitcom", released: true, episodes: 146, seasons: 7, episode_runtime: 30)
