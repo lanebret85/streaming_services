b334243cc0f24fa89183ad31304ab1c05610e4a1
Lane Bretschneider <lanebret85@gmail.com>
2023-08-25 18:06:13 -0700

    feat: update to test streaming services index view for changes with order_by_time_created method

--- spec/features/streaming_services/index_spec.rb
+++ spec/features/streaming_services/index_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe "Streaming Services Index", type: :feature do
   # Then I see the name of each parent record in the system
   describe "As a visitor" do
     describe "When I visit '/streaming_services'" do
-      it "can display the name of each streaming service record in the system" do
+      it "can display the name of each streaming_service in order of most recently created" do
         #arrange
         netflix = StreamingService.create!(name: "Netflix", subscribed: true, logged_in: true, rating: 42)
         hulu = StreamingService.create!(name: "Hulu", subscribed: true, logged_in: true, rating: 39)
@@ -16,9 +16,9 @@ RSpec.describe "Streaming Services Index", type: :feature do
         visit "/streaming_services"
         #assert
         expect(page).to have_content("All Streaming Services")
-        expect(page).to have_content(netflix.name)
-        expect(page).to have_content(hulu.name)
-        expect(page).to have_content(disney_plus.name)
+        expect(page).to have_content("#{netflix.name}, Created at: #{netflix.created_at}")
+        expect(page).to have_content("#{hulu.name}, Created at: #{hulu.created_at}")
+        expect(page).to have_content("#{disney_plus.name}, Created at: #{disney_plus.created_at}")
       end
     end
   end
