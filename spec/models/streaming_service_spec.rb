e01d182495c5cd736f3a2cc09d1d0e901e3805d6
Lane Bretschneider <lanebret85@gmail.com>
2023-08-25 18:04:40 -0700

    test: add before each with 3 services and model test for order_by_time_created method

--- spec/models/streaming_service_spec.rb
+++ spec/models/streaming_service_spec.rb
@@ -1,6 +1,12 @@
 require "rails_helper"
 
 RSpec.describe StreamingService, type: :model do
+  before :each do
+    @netflix = StreamingService.create!(name: "Netflix", subscribed: true, logged_in: true, rating: 42)
+    @hulu = StreamingService.create!(name: "Hulu", subscribed: true, logged_in: true, rating: 39)
+    @disney_plus = StreamingService.create!(name: "Disney Plus", subscribed: true, logged_in: false, rating: 35)
+  end
+  
   describe 'relationships' do
     it { should have_many :shows }
   end
@@ -12,5 +18,13 @@ RSpec.describe StreamingService, type: :model do
     it { should allow_value(true).for(:logged_in)}
     it { should allow_value(false).for(:logged_in)}
     it { should validate_presence_of :rating }
+  end
+
+  describe "#order_by_time_created" do
+    it "can display streaming services in order of most recently created" do
+      query = StreamingService.order(created_at: :desc)
+
+      expect(query).to eq([@disney_plus, @hulu, @netflix])
+    end
   end
 end
