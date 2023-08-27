5d8d744bd074c77127f22c45f39df39925efc85a
Lane Bretschneider <lanebret85@gmail.com>
2023-08-27 12:00:43 -0700

    test: update tests to reflect display_true method, where @is_it_cake has released: false and tests for page to not have content with its attributes

--- spec/features/shows/index_spec.rb
+++ spec/features/shows/index_spec.rb
@@ -14,11 +14,11 @@ RSpec.describe "Shows Index", type: :feature do
     describe "When I visit '/shows'" do
       before :each do
         @the_witcher = @netflix.shows.create!(name: "The Witcher", genre: "Fantasy", released: true, episodes: 24, seasons: 3, episode_runtime: 60)
-        @is_it_cake = @netflix.shows.create!(name: "Is it Cake?", genre: "Competition", released: true, episodes: 20, seasons: 2, episode_runtime: 60)
+        @is_it_cake = @netflix.shows.create!(name: "Is it Cake?", genre: "Competition", released: false, episodes: 20, seasons: 2, episode_runtime: 40)
         @new_girl = @hulu.shows.create!(name: "New Girl", genre: "Sitcom", released: true, episodes: 146, seasons: 7, episode_runtime: 30)
       end
 
-      it "can display all shows in the system with their attributes" do
+      it "can display all shows in the system with their attributes if the released column is true for that show" do
         # arrange
         
         # act
@@ -33,12 +33,12 @@ RSpec.describe "Shows Index", type: :feature do
         expect(page).to have_content("#{@the_witcher.episodes} Episodes")
         expect(page).to have_content("#{@the_witcher.episode_runtime} Minute Episodes")
         expect(page).to have_content("Streaming Service: #{@the_witcher.streaming_service_id}")
-        expect(page).to have_content(@is_it_cake.name)
-        expect(page).to have_content("Genre: #{@is_it_cake.genre}")
-        expect(page).to have_content("Released: #{@is_it_cake.released}")
-        expect(page).to have_content("#{@is_it_cake.seasons} Seasons")
-        expect(page).to have_content("#{@is_it_cake.episodes} Episodes")
-        expect(page).to have_content("#{@is_it_cake.episode_runtime} Minute Episodes")
+        expect(page).to_not have_content(@is_it_cake.name)
+        expect(page).to_not have_content("Genre: #{@is_it_cake.genre}")
+        expect(page).to_not have_content("Released: #{@is_it_cake.released}")
+        expect(page).to_not have_content("#{@is_it_cake.seasons} Seasons")
+        expect(page).to_not have_content("#{@is_it_cake.episodes} Episodes")
+        expect(page).to_not have_content("#{@is_it_cake.episode_runtime} Minute Episodes")
         expect(page).to have_content("Streaming Service: #{@is_it_cake.streaming_service_id}")
         expect(page).to have_content(@new_girl.name)
         expect(page).to have_content("Genre: #{@new_girl.genre}")
