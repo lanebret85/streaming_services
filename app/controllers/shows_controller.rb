0072661bcb666a1366f9b960ae96dde35b4edd01
Lane Bretschneider <lanebret85@gmail.com>
2023-08-27 11:15:28 -0700

    feat: add edit and update actions, as well as show_params private method

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -32,4 +32,19 @@ class ShowsController < ApplicationController
     #   @streaming_service = "Disney Plus"
     # end
   end
+
+  def edit
+    @show = Show.find(params[:id])
+  end
+
+  def update
+    show = Show.find(params[:id])
+    show.update(show_params)
+    redirect_to "/shows/#{show.id}"
+  end
+
+private
+  def show_params
+    params.permit(:name, :genre, :released, :episodes, :seasons, :episode_runtime)
+  end
 end
