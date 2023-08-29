bdb9421588cec039dd182b418d23d36b56385a65
Lane Bretschneider <lanebret85@gmail.com>
2023-08-29 15:49:57 -0700

    refactor: remove old commented out code

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,19 +1,6 @@
 class ShowsController < ApplicationController
   def index
     @shows = Show.all
-
-    # if Show.find(params[:id]).released
-    #   @released_y_n = "Yes"
-    # else
-    #   @released_y_n = "No"
-    # end
-    # if Show.find(params[:id]).streaming_service_id == 1
-    #   @streaming_service = "Netflix"
-    # elsif Show.find(params[:id]).streaming_service_id == 2
-    #   @streaming_service = "Hulu"
-    # elsif Show.find(params[:id]).streaming_service_id == 3
-    #   @streaming_service = "Disney Plus"
-    # end
   end
 
   def show
@@ -23,14 +10,6 @@ class ShowsController < ApplicationController
     else
       @released_y_n = "No"
     end
-    # binding.pry
-    # if @show.streaming_service_id == 1
-    #   @streaming_service = "Netflix"
-    # elsif @show.streaming_service_id == 2
-    #   @streaming_service = "Hulu"
-    # elsif @show.streaming_service_id == 0
-    #   @streaming_service = "Disney Plus"
-    # end
   end
 
   def edit
